0e73956ee1b0437a6d92b7c6fc164bdb95093307
Woo KIM <ssw890720@gmail.com>
2019-12-30 00:17:06 +0900

    fixed untracked files

--- src/main/java/com/song/woo/todo/dao/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.song.woo.todo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.song.woo.todo.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-}
