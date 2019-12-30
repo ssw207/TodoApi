ae305076c19e1d4f74c95b911a9dde965e668a52
Woo KIM <ssw890720@gmail.com>
2019-12-31 00:25:55 +0900

    1.로그파일 gitignore추가 2.컨트롤러 전역 예외처리 기능 추가

--- src/test/java/com/song/woo/member/MemberControllerTest.java
+++ src/test/java/com/song/woo/member/MemberControllerTest.java
@@ -1,8 +1,10 @@
 package com.song.woo.member;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -11,8 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.song.woo.member.model.Member;
+import com.song.woo.member.service.MemberService;
 
 @RunWith(SpringRunner.class)
 /* @Controller, @RestController가 설정된 클래스를 메모리에 생성(@Service, @Repository 생성X) */
@@ -28,16 +35,21 @@ public class MemberControllerTest {
 	@Autowired
 	private MockMvc mockMvc; // 서블릿 컨테이너를 모방한 테스트 객체
 	
+	@MockBean
+	private MemberService s;
+	
 	@Test
 	public void 컨트롤러테스트() throws Exception {
-		mockMvc.perform(post("http://localhost:8081/api/member/save") // url요청
-							.param("memId", "test") 
-							.param("memPw", "1234")
-							.param("nmName", "1234"))
+		//given
+		Member member= Member.builder().memId("admin").memPw("1234").memName("admin2").build();
+		
+		when(s.memberSave(member))
+			.thenReturn(member);
+		
+		mockMvc.perform(post("/api/member/save") // url요청
+				.accept(MediaType.APPLICATION_JSON))			
 				.andExpect(status().isOk()) // 결과
-				.andExpect(model().attribute("memId", "test"))
-				.andExpect(model().attribute("memPw", "1234"))
-				.andExpect(model().attribute("nmName", "1234"))
+				.andExpect(jsonPath("$.memId").value("admin"))
 				.andDo(print());
 	}
 }
